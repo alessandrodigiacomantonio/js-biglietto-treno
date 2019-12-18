277783057d752901271df28efbd681fbf60066cd
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2019-12-18 16:11:41 +0100

    Script.js: aggiunto un .toFixed mancante

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ if (dis < 1 || dis > 500) {
   alert('Purtroppo hai inserito delle distanze non coperte dal nostro servizio di trasporti, ci spiace !')
 } else {
     var distanza = dis.match(/\d+/);
-    console.log(distanza);
+    distanza = Math.floor(distanza);
     var prezzoDefault = distanza * 0.21;
     var eta = prompt("Qual è la tua età?");
     if (eta > 122) {
@@ -13,7 +13,7 @@ if (dis < 1 || dis > 500) {
     var prezzo = (prezzoDefault - (prezzoDefault * 40 / 100)).toFixed(2);
     alert('Il prezzo del biglietto è di \n' + '€ ' + prezzo +'\n Grazie per aver scelto il nostro servizio e Buon Viaggio');
 } else if (eta < 65 && eta > 17) {
-    alert('il prezzo del biglietto è di \n' + '€ ' + prezzoDefault + '\n Grazie per aver scelto il nostro servizio e Buon Viaggio');
+    alert('il prezzo del biglietto è di \n' + '€ ' + prezzoDefault.toFixed(2) + '\n Grazie per aver scelto il nostro servizio e Buon Viaggio');
 } else if (eta <= 17 && eta > 2) {
       var prezzo = (prezzoDefault - (prezzoDefault * 20 / 100)).toFixed(2);
       alert('Il prezzo del biglietto è di \n' + '€ ' + prezzo +'\n   Grazie per aver scelto il nostro servizio e Buon Viaggio');
