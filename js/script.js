ac77a3f0d3282021bb34c8e1f42730b2a21e3988
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2019-12-18 17:09:59 +0100

    Script.js: migliorata (si spera) indentatura

--- js/script.js
+++ js/script.js
@@ -10,18 +10,18 @@ if (distanza < 1 || distanza > 500 || isNaN(distanza)) {
     var anni = eta.match(/\d+/);
     anni = Math.floor(anni);
     if (isNaN(anni)) {
-    alert('Non hai inserito un\'età valida !');
+      alert('Non hai inserito un\'età valida !');
     } else if (anni > 122) {
-      alert('Sarebbe fantastico essere così longevi, ma hai un\'età maggiore della persona più anziana di tutti i tempi, il che mi sembra improbabile :D');
+        alert('Sarebbe fantastico essere così longevi, ma hai un\'età maggiore della persona più anziana di tutti i tempi, il che mi sembra improbabile :D');
     } else if (anni >= 65) {
-      var prezzo = (prezzoDefault - (prezzoDefault * 40 / 100)).toFixed(2);
-      alert('Il prezzo del biglietto è di \n' + '€ ' + prezzo +'\n Grazie per aver scelto il nostro servizio e Buon Viaggio');
+        var prezzo = (prezzoDefault - (prezzoDefault * 40 / 100)).toFixed(2);
+        alert('Il prezzo del biglietto è di \n' + '€ ' + prezzo +'\n Grazie per aver scelto il nostro servizio e Buon Viaggio');
     } else if (anni < 65 && eta > 17) {
-      alert('il prezzo del biglietto è di \n' + '€ ' + prezzoDefault.toFixed(2) + '\n Grazie per aver scelto il nostro servizio e Buon Viaggio');
+        alert('il prezzo del biglietto è di \n' + '€ ' + prezzoDefault.toFixed(2) + '\n Grazie per aver scelto il nostro servizio e Buon Viaggio');
     } else if (anni <= 17 && anni > 2) {
         var prezzo = (prezzoDefault - (prezzoDefault * 20 / 100)).toFixed(2);
         alert('Il prezzo del biglietto è di \n' + '€ ' + prezzo +'\n   Grazie per aver scelto il nostro servizio e Buon Viaggio');
     } else {
-          alert('I bebè, fino a 2 anni, viaggiano gratis !');
+        alert('I bebè, fino a 2 anni, viaggiano gratis !');
     }
 }
